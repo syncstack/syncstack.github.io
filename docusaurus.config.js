b936c854cab3829c08b5ca84992770d267c888c8
SyncStack Dev <syncstack-dev@gmail.com>
2020-04-20 18:39:27 +0000

    Download page created.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/syncstack/syncstack.github.io/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
